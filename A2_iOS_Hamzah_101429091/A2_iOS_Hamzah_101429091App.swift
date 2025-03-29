60a45552127a34e94289da45951bb393d6b81f6a
Hamzah Hafez <Hamzahafez@gmail.com>
2025-03-28 23:32:33 -0400

    Initial Commit

---
+++ A2_iOS_Hamzah_101429091/A2_iOS_Hamzah_101429091App.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_Hamzah_101429091App.swift
+//  A2_iOS_Hamzah_101429091
+//
+//  Created by Hamza hafez on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct A2_iOS_Hamzah_101429091App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A2_iOS_Hamzah_101429091/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A2_iOS_Hamzah_101429091
+//
+//  Created by Hamza hafez on 2025-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
